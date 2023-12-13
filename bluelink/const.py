eaba79d7ccd8bc48247c4fc99bee50512b30a0f6
TaiPhamD <peterpham86@gmail.com>
2023-12-12 19:45:45 -0800

    test branch

--- bluelink/const.py
+++ bluelink/const.py
@@ -1,6 +1,6 @@
 # custom_components/bluelink_climate/const.py
 
-DOMAIN = "bluelink_climate"
+DOMAIN = "bluelink_api_hacs"
 
 # max bluelink climate control can run is 10 minutes (600 seconds)
 BLUELINK_CLIMATE_MAX_TIMER = 600
